3907e633030cd078c245e6afd319c5c5ab391207
Nabeel Mumtaz <153012098+nabeelmumtaz92@users.noreply.github.com>
2024-12-24 21:13:42 -0600

    Add files via upload

---
+++ StarsList.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+
+
+export default function StarsList() {
+  const [stars, setStars] = useState([])
+  const navigate = useNavigate ()
+
+  const logout = () => {
+    localStorage.removeItem('token')
+    navigate('/')
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+    if (!token) {
+      logout()
+    } else {
+      const fetchStars = async () => {
+        try {
+          const response = await axios.get (
+            '/api/stars',
+            { headers: { Authorization: token } } 
+          )
+            setStars(response.data)
+        } catch (error) {
+          if (error?.response?.statut == 401) {
+            logout()
+          }
+        }
+      }
+      fetchStars()
+    }
+    }
+
+  })
+
+  return (
+    <div className="container">
+      <h3>StarsList <button>Logout</button></h3>
+      {stars.length > 0 ? (
+        <div>
+          {stars.map((star) => (
+            <div key={star.id} style={{ marginBottom: '20px' }} className="star">
+              <h4>{star.fullName}</h4>
+              <p>Born: {star.born}</p>
+              <p>{star.bio}</p>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p>No stars found.</p>
+      )}
+    </div>
+  )
+}
